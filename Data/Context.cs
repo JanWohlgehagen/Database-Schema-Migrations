090cec4e8edead709c21d23d5ba9f04c4cba7df0
Theut94 <Mikkeltheut@gmail.com>
2024-02-26 12:55:10 +0100

    Feat: initial model

---
+++ Data/Context.cs
@@ -0,0 +1,20 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class Context : DbContext
+    {
+        public string DbPath { get; set; }
+        public Context()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "EShop.db");
+        }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+
+    }
+}
