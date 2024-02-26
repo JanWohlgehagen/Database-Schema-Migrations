1bd99ef44f8f3dc81277da9579530e30803e35eb
Tadiaki <n0m1z@hotmail.com>
2024-02-26 12:58:11 +0100

    Fix: naming convention to match original Product

--- Manual migrations/v1.2__26022024_manual_add_ratings.sql
+++ Manual migrations/v1.2__26022024_manual_add_ratings.sql
@@ -4,5 +4,5 @@ CREATE TABLE ProductRatings (
     ProductID INT NOT NULL,
     Rating INT NOT NULL,
     Review NVARCHAR(MAX),
-    CONSTRAINT FK_ProductRatings_ProductID FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+    CONSTRAINT FK_ProductRatings_ProductID FOREIGN KEY (ProductID) REFERENCES Products(Id)
 );
